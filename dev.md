d645cbbdec269546ebfb1fef9c0812a193fca8aa
abdulsalamamtech <andulsalamamtech@gmail.com>
2024-10-08 19:31:49 +0100

    first commit

---
+++ dev.md
@@ -0,0 +1,7 @@
+## Development Extensions (34)
+
+- Prettier
+- Tailwindcss Kit
+- Laravel Extension Pack
+- Codeium AI & Tabnine
+- Laravel Blade Spacer
---
+++ resources/views/dashboard/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!--
+
+ * DEVELOPER : Abdulsalam Amtech
+ * EMAIL: abdulsalamamtech@gmail.com
+
+-->
+
+<!-- Header -->
+@include('dashboard.partials.header')
+
+
+<!-- Navigation -->
+@include('dashboard.partials.index-navbar')
+
+
+
+@yield('content')
+
+
+
+<!-- Footer -->
+@include('dashboard.partials.footer')
